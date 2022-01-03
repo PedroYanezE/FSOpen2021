84745f857c50a51f56dfc16499b0e4b9e0f91dd7
PedroYanezE <pedro.yanez.e@gmail.com>
2022-01-03 16:30:39 -0300

    unicafe step4

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -31,24 +31,39 @@ const App = () => {
     setBad(bad + 1)
   }
 
-  return(
-    <div>
-      <Heading text = "give feedback"/>
+  if (good + neutral + bad)
+    return(
+      <div>
+        <Heading text = "give feedback"/>
 
-      <Button handleFunction = {handleGood} text = "good"/>
-      <Button handleFunction = {handleNeutral} text = "neutral"/>
-      <Button handleFunction = {handleBad} text = "bad"/>
+        <Button handleFunction = {handleGood} text = "good"/>
+        <Button handleFunction = {handleNeutral} text = "neutral"/>
+        <Button handleFunction = {handleBad} text = "bad"/>
 
-      <Heading text = "statistics"/>
+        <Heading text = "statistics"/>
 
-      <Statistics text = "good" value = {good}/>
-      <Statistics text = "neutral" value = {neutral}/>
-      <Statistics text = "bad" value = {bad}/>
-      <Statistics text = "all" value = {good + neutral + bad}/>
-      <Statistics text = "average" value = {(good - bad)/(good + bad + neutral)}/>
-      <Statistics text = "positive" value = {((good)/(good + bad + neutral)) * 100}/>
-    </div>
+        <Statistics text = "good" value = {good}/>
+        <Statistics text = "neutral" value = {neutral}/>
+        <Statistics text = "bad" value = {bad}/>
+        <Statistics text = "all" value = {good + neutral + bad}/>
+        <Statistics text = "average" value = {(good - bad)/(good + bad + neutral)}/>
+        <Statistics text = "positive" value = {((good)/(good + bad + neutral)) * 100}/>
+      </div>
   )
+  else 
+    return(
+      <div>
+        <Heading text = "give feedback"/>
+
+        <Button handleFunction = {handleGood} text = "good"/>
+        <Button handleFunction = {handleNeutral} text = "neutral"/>
+        <Button handleFunction = {handleBad} text = "bad"/>
+
+        <Heading text = "statistics"/>
+
+        <p>No feedback given</p>
+      </div>
+    )
 }
 
 export default App;
